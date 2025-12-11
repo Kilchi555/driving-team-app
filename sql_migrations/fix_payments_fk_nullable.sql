6320221100699a482e74fda5d3c50111f833f03e
Kilchi555 <pascal_kilchenmann@icloud.com>
2025-12-11 10:32:55 +0100

    Fix: Drop both old constraints before recreating with unique name
    Änderungen:
    - Drop payments_company_billing_address_id_fkey1 (alte constraint)
    - Drop payments_company_billing_address_id_fkey (neue constraint falls sie existiert)
    - Erstelle neue constraint mit Namen: payments_company_billing_address_id_fkey_nullable
    - Eindeutiger Name verhindert Konflikte
    
    Problem war:
    - payments_company_billing_address_id_fkey existierte bereits
    - Konnte nicht mit gleichem Namen recreated werden
    
    Lösung:
    - Neue eindeutige Name: _fkey_nullable
    - Beide alte Constraints werden gedroppt
    - Nur die neue wird erstellt

--- sql_migrations/fix_payments_fk_nullable.sql
+++ sql_migrations/fix_payments_fk_nullable.sql
@@ -1,14 +1,17 @@
 -- Fix foreign key constraint to allow null company_billing_address_id in payments table
 -- Issue: Foreign key constraint is too strict and doesn't allow null values
 
--- Drop the existing foreign key constraint
+-- Drop ALL existing foreign key constraints for this column
 ALTER TABLE payments 
 DROP CONSTRAINT IF EXISTS "payments_company_billing_address_id_fkey1";
+
+ALTER TABLE payments 
+DROP CONSTRAINT IF EXISTS "payments_company_billing_address_id_fkey";
 
 -- Recreate the foreign key constraint that allows null values
 -- A null value in company_billing_address_id will not trigger the FK check
 ALTER TABLE payments
-ADD CONSTRAINT "payments_company_billing_address_id_fkey"
+ADD CONSTRAINT "payments_company_billing_address_id_fkey_nullable"
 FOREIGN KEY (company_billing_address_id)
 REFERENCES company_billing_addresses(id)
 ON DELETE SET NULL
@@ -18,5 +21,4 @@ ON UPDATE CASCADE;
 -- SELECT constraint_name, table_name, column_name
 -- FROM information_schema.key_column_usage
 -- WHERE table_name = 'payments' AND column_name = 'company_billing_address_id';
-
 
