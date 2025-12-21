6f201a9fd41fb660a734164535dfea60aee7369f
Kilchi555 <pascal_kilchenmann@icloud.com>
2025-12-21 07:06:27 +0100

    chore: additional changes to evaluation modal

--- components/EvaluationModal.vue
+++ components/EvaluationModal.vue
@@ -138,7 +138,7 @@
                   v-model="criteriaNotes[criteriaId]"
                   :placeholder="`Notiz optional...`"
                   class="w-full p-2 text-sm border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent resize-none overflow-y-auto"
-                  style="height: 4.5rem; max-height: 10rem;"
+                  style="height: 2.5rem; max-height: 10rem;"
                   rows="1"
                 ></textarea>
               </div>
