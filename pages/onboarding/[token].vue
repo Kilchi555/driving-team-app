60b37ebe9338978f33d2044b51c37b315eb0f4ad
Kilchi555 <pascal_kilchenmann@icloud.com>
2025-12-23 15:52:43 +0100

    fix: Category selection not working in onboarding
    - Changed label to div to prevent event propagation issues
    - Added @click.stop to prevent checkbox click conflicts
    - Now clicking anywhere on category box toggles selection properly
    - Checkbox state syncs correctly with v-model binding

--- pages/onboarding/[token].vue
+++ pages/onboarding/[token].vue
@@ -295,7 +295,7 @@
                     v-for="cat in categories" 
                     :key="cat.code || cat.id" 
                     class="flex justify-between p-4 bg-gray-50 rounded-lg border border-gray-200 hover:border-green-300 transition-colors cursor-pointer"
-                    @click="toggleCategory(cat.code || cat.id)"
+                    @click.stop="toggleCategory(cat.code || cat.id)"
                   >
                     <div class="flex-1 min-w-0">
                       <div class="flex items-center space-x-3">
@@ -303,7 +303,7 @@
                         <span class="text-sm text-gray-600">{{ cat.name }}</span>
                       </div>
                     </div>
-                    <label class="relative inline-flex items-start cursor-pointer ml-4 flex-shrink-0">
+                    <div class="relative inline-flex items-start cursor-pointer ml-4 flex-shrink-0" @click.stop>
                       <input
                         v-model="form.categories"
                         :value="cat.code || cat.id"
@@ -312,7 +312,7 @@
                         @change="clearCategoryError"
                       />
                       <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-green-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600"></div>
-                    </label>
+                    </div>
                   </div>
                 </div>
 
